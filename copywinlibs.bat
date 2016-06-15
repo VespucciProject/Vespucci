5c151efdaab570304520bc5e3b8bd122493e8fb2
Daniel Foose <dpfoose@gmail.com>
2016-06-14 20:41:32 -0400

    new windows build configuration

---
+++ copywinlibs.bat
@@ -0,0 +1,6 @@
+IF NOT EXIST "Vespucci\release\libgcc_s_seh-1.dll" copy /y "..\Vespucci_dependencies\DLL\libgcc_s_seh-1.dll" "Vespucci\release"
+IF NOT EXIST "Vespucci\release\libgfortran-3.dll" copy /y "..\Vespucci_dependencies\DLL\libgfortran-3.dll" "Vespucci\release"
+IF NOT EXIST "Vespucci\release\libopenblas.dll" copy /y "..\Vespucci_dependencies\DLL\libopenblas.dll" "Vespucci\release"
+IF NOT EXIST "Vespucci\release\libquadmath-0.dll" copy /y "..\Vespucci_dependencies\DLL\libquadmath-0.dll" "Vespucci\release"
+IF NOT EXIST "Vespucci\release\lapack_x64.dll" copy /y "..Vespucci_dependencies\DLL\lapack_x64.dll"
+copy /y "VespucciLibrary\release\vespucci.dll" "Vespucci\release"
