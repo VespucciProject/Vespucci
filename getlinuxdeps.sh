b94865ed64799d778f808c508ee8e6d690d6f087
Daniel Foose <dpfoose@gmail.com>
2016-06-21 17:11:53 -0400

    Updated to Qt 5.6 on Travis-CI linux builds.

--- getlinuxdeps.sh
+++ getlinuxdeps.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-wget https://github.com/VespucciProject/Vespucci-QCP/releases/download/1.3.1/vespucci_travis_depts.tar.gz
-tar xvf vespucci_travis_depts.tar.gz -C /home/travis
+wget https://github.com/VespucciProject/Vespucci_dependencies/releases/download/1/Vespucci_dependencies_linux.tar.gz
+tar xvf Vespucci_dependencies_linux.tar.gz -C /home/travis
 sudo apt-add-repository -y ppa:ubuntu-sdk-team
 sudo apt-add-repository -y ppa:ubuntu-toolchain-r/test
-sudo apt-add-repository -y ppa:beineri/opt-qt551-trusty
+sudo apt-add-repository -y ppa:beineri/opt-qt561-trusty
 sudo apt-get -y update
-sudo apt-get -y install qt55base qt55tools qt55imageformats qt55location qt55declarative libsqlite3-dev qt55svg
+sudo apt-get -y install qt56base qt56tools qt56imageformats qt56location qt56declarative libsqlite3-dev qt56svg
 sudo apt-get -y install libhdf5-dev libarpack2-dev
 sudo apt-get -y install libxml2-dev libboost1.55-dev libboost-math1.55-dev libboost-program-options1.55-dev libboost-test1.55-dev libboost-random1.55-dev libcminpack-dev
-source /opt/qt55/bin/qt55-env.sh
+source /opt/qt56/bin/qt56-env.sh
