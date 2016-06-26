2c30b076ef4c34c5cc4679fff30007c7ae0d05b6
Daniel Foose <dpfoose@gmail.com>
2016-06-25 22:55:36 -0400

    Forgot a -y flag on apt-get.

--- getlinuxdeps.sh
+++ getlinuxdeps.sh
@@ -5,7 +5,7 @@ sudo apt-add-repository -y ppa:ubuntu-sdk-team
 sudo apt-add-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-add-repository -y ppa:beineri/opt-qt561-trusty
 sudo apt-get -y update
-sudo apt-get install g++-4.9
+sudo apt-get -y install g++-4.9
 sudo apt-get -y install qt56base qt56tools qt56imageformats qt56location qt56declarative libsqlite3-dev qt56svg
 sudo apt-get -y install libhdf5-dev libarpack2-dev
 sudo apt-get -y install libxml2-dev libboost1.55-dev libboost-math1.55-dev libboost-program-options1.55-dev libboost-test1.55-dev libboost-random1.55-dev
