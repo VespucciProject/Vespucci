35cbb35b7b78626306854e78136876e4ee208856
Daniel Foose <dpfoose@gmail.com>
2016-06-26 20:11:42 -0400

    installing all boost (will be cached by Travis CI anyway)

--- getlinuxdeps.sh
+++ getlinuxdeps.sh
@@ -5,9 +5,11 @@ sudo apt-add-repository -y ppa:ubuntu-sdk-team
 sudo apt-add-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-add-repository -y ppa:beineri/opt-qt561-trusty
 sudo apt-get -y update
-sudo apt-get -y install g++-4.9
-sudo apt-get -y install qt56base qt56tools qt56imageformats qt56location qt56declarative libsqlite3-dev qt56svg
+sudo apt-get -y install g++-4.9 build-essential
+sudo apt-get -y install qt56base qt56tools qt56imageformats 
+sudo apt-get -y install qt56location qt56declarative libsqlite3-dev qt56svg
 sudo apt-get -y install mesa-common-dev freeglut3.dev
 sudo apt-get -y install libhdf5-dev libarpack2-dev
-sudo apt-get -y install libxml2-dev libboost1.55-dev libboost-math1.55-dev libboost-program-options1.55-dev libboost-test1.55-dev libboost-random1.55-dev
+sudo apt-get -y install libxml2-dev libboost1.55-dev libboost-math1.55-dev 
+sudo apt-get -y install libboost1.55-all-dev
 sudo apt-get -y install zlib1g zlib1g-dev
