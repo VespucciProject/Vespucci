78427ea46b18ea7e72e445279ab2f04ccd6a23bb
Daniel Foose <dpfoose@gmail.com>
2016-08-24 14:03:44 -0400

    fixed syntax error

--- getosxdeps.sh
+++ getosxdeps.sh
@@ -13,17 +13,17 @@ if [ ! -f /usr/local/lib/libarpack.dylib ]; then
     brew install arpack
     brew link arpack
 fi
-if [! -f /usr/local/lib/libxml2.dylib ]; then
+if [ ! -f /usr/local/lib/libxml2.dylib ]; then
     brew install libxml2
     brew link libxml2
 fi
-if [! -f /usr/local/bin/wget ]; then
+if [ ! -f /usr/local/bin/wget ]; then
     brew install wget
     brew link wget
 fi
-if [! -f /usr/local/include/superlu/supermatrix.h ]; then
+if [ ! -f /usr/local/include/superlu/supermatrix.h ]; then
     brew install superlu43
-    brew link superlu43
+    brew link superlu43 --force
 fi
 wget https://github.com/VespucciProject/Vespucci_dependencies/releases/download/1/Vespucci_dependencies_macOS.zip
 unzip Vespucci_dependencies_macOS.zip
