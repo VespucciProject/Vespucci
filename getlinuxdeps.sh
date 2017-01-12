dfad9e8ec01f7b9b07142a50d5c3adc500627568
Daniel Patrick Foose <dpfoose@gmail.com>
2017-01-12 12:01:03 -0500

    Update getlinuxdeps.sh

--- getlinuxdeps.sh
+++ getlinuxdeps.sh
@@ -3,11 +3,11 @@ wget https://github.com/VespucciProject/Vespucci_dependencies/releases/download/
 tar xvf Vespucci_dependencies_linux.tar.gz
 sudo apt-add-repository -y ppa:ubuntu-sdk-team
 sudo apt-add-repository -y ppa:ubuntu-toolchain-r/test
-sudo apt-add-repository -y ppa:beineri/opt-qt561-trusty
+sudo apt-add-repository -y ppa:beineri/opt-qt57-trusty
 sudo apt-get -y update
 sudo apt-get -y install g++-4.9 build-essential
-sudo apt-get -y install qt56base qt56tools qt56imageformats 
-sudo apt-get -y install qt56location qt56declarative libsqlite3-dev qt56svg
+sudo apt-get -y install qt57base qt57tools qt57imageformats 
+sudo apt-get -y install qt57location qt57declarative libsqlite3-dev qt57svg
 sudo apt-get -y install mesa-common-dev freeglut3.dev
 sudo apt-get -y install libarpack2-dev
 sudo apt-get -y install libxml2-dev
