cc14d60320c9a54c8582354b46bcb5df9879b906
Daniel Foose <dpfoose@gmail.com>
2017-02-16 11:49:18 -0500

    updated copyright notice

--- Vespucci/Data/Dataset/vespuccidataset.cpp
+++ Vespucci/Data/Dataset/vespuccidataset.cpp
@@ -1,5 +1,5 @@
  /*******************************************************************************
-    Copyright (C) 2014-2016 Wright State University - All Rights Reserved
+    Copyright (C) 2014-2017 Wright State University - All Rights Reserved
     Daniel P. Foose - Maintainer/Lead Developer
 
     This file is part of Vespucci.
