9ed651c245adf167f3e22da7eca4a0b51edc8d2c
Daniel Foose <dpfoose@gmail.com>
2017-02-16 11:51:31 -0500

    capitalization issue fixed

--- Vespucci/GUI/QAbstractItemModel/filetablemodel.cpp
+++ Vespucci/GUI/QAbstractItemModel/filetablemodel.cpp
@@ -1,24 +1,35 @@
+/*******************************************************************************
+    Copyright (C) 2014-2017 Wright State University - All Rights Reserved
+    Daniel P. Foose - Maintainer/Lead Developer
+
+    This file is part of Vespucci.
+
+    Vespucci is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Vespucci is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Vespucci.  If not, see <http://www.gnu.org/licenses/>.
+*******************************************************************************/
 #include "filetablemodel.h"
 
-filetablemodel::filetablemodel(QObject *parent)
+FileTableModel::filetablemodel(QObject *parent)
     : QAbstractTableModel(parent)
 {
 }
 
-QVariant filetablemodel::headerData(int section, Qt::Orientation orientation, int role) const
+QVariant FileTableModel::headerData(int section, Qt::Orientation orientation, int role) const
 {
     // FIXME: Implement me!
 }
 
-int filetablemodel::rowCount(const QModelIndex &parent) const
-{
-    if (parent.isValid())
-        return 0;
-
-    // FIXME: Implement me!
-}
-
-int filetablemodel::columnCount(const QModelIndex &parent) const
+int FileTableModel::rowCount(const QModelIndex &parent) const
 {
     if (parent.isValid())
         return 0;
@@ -26,7 +37,15 @@ int filetablemodel::columnCount(const QModelIndex &parent) const
     // FIXME: Implement me!
 }
 
-QVariant filetablemodel::data(const QModelIndex &index, int role) const
+int FileTableModel::columnCount(const QModelIndex &parent) const
+{
+    if (parent.isValid())
+        return 0;
+
+    // FIXME: Implement me!
+}
+
+QVariant FileTableModel::data(const QModelIndex &index, int role) const
 {
     if (!index.isValid())
         return QVariant();
@@ -35,28 +54,28 @@ QVariant filetablemodel::data(const QModelIndex &index, int role) const
     return QVariant();
 }
 
-bool filetablemodel::insertRows(int row, int count, const QModelIndex &parent)
+bool FileTableModel::insertRows(int row, int count, const QModelIndex &parent)
 {
     beginInsertRows(parent, row, row + count - 1);
     // FIXME: Implement me!
     endInsertRows();
 }
 
-bool filetablemodel::insertColumns(int column, int count, const QModelIndex &parent)
+bool FileTableModel::insertColumns(int column, int count, const QModelIndex &parent)
 {
     beginInsertColumns(parent, column, column + count - 1);
     // FIXME: Implement me!
     endInsertColumns();
 }
 
-bool filetablemodel::removeRows(int row, int count, const QModelIndex &parent)
+bool FileTableModel::removeRows(int row, int count, const QModelIndex &parent)
 {
     beginRemoveRows(parent, row, row + count - 1);
     // FIXME: Implement me!
     endRemoveRows();
 }
 
-bool filetablemodel::removeColumns(int column, int count, const QModelIndex &parent)
+bool FileTableModel::removeColumns(int column, int count, const QModelIndex &parent)
 {
     beginRemoveColumns(parent, column, column + count - 1);
     // FIXME: Implement me!
--- Vespucci/GUI/QAbstractItemModel/filetablemodel.h
+++ Vespucci/GUI/QAbstractItemModel/filetablemodel.h
@@ -3,12 +3,12 @@
 
 #include <QAbstractTableModel>
 
-class filetablemodel : public QAbstractTableModel
+class FileTableModel : public QAbstractTableModel
 {
     Q_OBJECT
 
 public:
-    explicit filetablemodel(QObject *parent = 0);
+    explicit FileTableModel(QObject *parent = 0);
 
     // Header:
     QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
