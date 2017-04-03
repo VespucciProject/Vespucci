5bb518e86af7d60e128f5b0973d51f97c002bbde
Daniel Patrick Foose <dpfoose@gmail.com>
2017-04-03 13:26:32 -0400

    Update libvespucci.h
    indentation

--- VespucciLibrary/include/libvespucci.h
+++ VespucciLibrary/include/libvespucci.h
@@ -1,12 +1,12 @@
 #ifndef LIBVESPUCCI_H
 #define LIBVESPUCCI_H
-#if VESPUCCI_LIBRARY 
-#define VESPUCCI_EXPORT __attribute__((__visibility__("default")))
-#elif VESPUCCI_LIBRARY && defined _MSC_VER
-#define VESPUCCI_EXPORT  __declspec(dllexport)
-#elif defined _MSC_VER
-#define VESPUCCI_EXPORT __declspec(dllimport)
-#else
-#define VESPUCCI_EXPORT 
-#endif // VESPUCCI_LIBRARY
+  #if VESPUCCI_LIBRARY 
+    #define VESPUCCI_EXPORT __attribute__((__visibility__("default")))
+  #elif VESPUCCI_LIBRARY && defined _MSC_VER
+    #define VESPUCCI_EXPORT __declspec(dllexport)
+  #elif defined _MSC_VER
+    #define VESPUCCI_EXPORT __declspec(dllimport)
+  #else
+    #define VESPUCCI_EXPORT 
+  #endif // VESPUCCI_LIBRARY
 #endif // LIBESPUCCI_H
